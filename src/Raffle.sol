c0b474f51777a0b843f4a0d0cc2f46300b5011eb
jyh-centos9 <568000273@qq.com>
2024-08-29 11:05:08 +0800

    going to use VRF v2.5

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,30 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/**ERRORS */
-error Raffle__NotEnoughToEntrance;
-
 /**
  * @title Raffle
  * @author jyh
- * @notice A comprehensive contract 
+ * @notice A comprehensive contract
  */
 contract Raffle {
-    uint256 private immutable i_entrancePrice;
+    /**ERRORS */
+    error Raffle__NotEnoughToEntrance();
 
-    constructor(uint256 entrance) {
+    uint256 private immutable i_entrancePrice;
+    // @dev duration of the lottery last (unit "s")
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimestamp;
+    address payable[] private s_players;
+
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entrance, uint256 interval) {
         i_entrancePrice = entrance;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
     }
 
     function enterRaffle() public payable {
         if (msg.value < i_entrancePrice) {
             revert Raffle__NotEnoughToEntrance();
         }
+
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    // 1. generate a random number
+    // 2. use the random number to pick a winner
+    // 3. automatic pick with the interval
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert();
+        }
 
-    function getEntrancePrice() external returns (uint256) {
+        // get a random number from chainlink VRF v2.5
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    function getEntrancePrice() external view returns (uint256) {
         return i_entrancePrice;
     }
 }
